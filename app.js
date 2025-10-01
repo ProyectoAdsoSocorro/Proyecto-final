cb7a659fe7601d9732b8f2f4f4d3830c7c6ecbdd
Eric-2804 <ericdayan13@gmail.com>
2025-10-01 18:26:10 -0500

    revision conflicto app.js

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import periodoRoutes from "./routes/periodRoutes.js";
+
+import periodRoutes from "./routes/periodRoutes.js";
+//import validityRoutes from "./routes/validityRoutes.js";
 
 dotenv.config();
 const app = express();
+
+// 📌 Configuración
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
 
 // 📌 Middlewares
 app.use(express.json());
 
 // 📌 Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -18,10 +24,10 @@ mongoose.connect(process.env.MONGO_URL, {
 .catch((err) => console.error("❌ Error de conexión a MongoDB:", err));
 
 // 📌 Rutas principales
-app.use("/api/periodos", periodoRoutes);
+app.use("/api/periodos", periodRoutes);
+//app.use("/api/validities", validityRoutes);
 
 // 📌 Servidor
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor en ejecución: http://localhost:${PORT}/api/periodos`);
+  console.log(`🚀 Servidor en ejecución: http://localhost:${PORT}`);
 });
