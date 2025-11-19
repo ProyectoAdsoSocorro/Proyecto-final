6cb53b9ff6a1f9c7b0403393e87d15fb3b8b5bbb
Juliana <Karenjuliana.co@gmail.com>
2025-11-19 12:47:58 -0500

    arreglando bug

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ import colegiosRoutes from './routes/schools.js';
 import registration from "./routes/registration.js";
 import headquartersRoutes from './routes/headquarters.js';
 
-import reportesEstudiantes from './routes/reports2_routes.js'
-import auth from './routes/auth.js'
+// import reportesEstudiantes from './routes/reports2_routes.js'
 
-import reportesEstudiantes from './routes/students_by_group.js';
+
+// import reportesEstudiantes from './routes/students_by_group.js';
 import students_by_group from './routes/students_by_group.js';
 import qualifications from './models/qualifications.js';
 import validity from './models/validity.js';
