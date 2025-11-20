6a5f7288731c096c7213db6704f8b233337c1385
Beto-12 <carlosalbertodb42@gmail.com>
2025-11-20 17:37:39 -0500

    Actualizando rutas de materia

--- routes/subjects.js
+++ routes/subjects.js
@@ -34,25 +34,21 @@ router.get('/area/:areaCode',[
 ], listByArea);             // GET /api/subjects/area/:areaCode
 
 router.post('/',[
-  check('school', 'Validación: ID de colegio debe ser válido').isMongoId().not().isEmpty().trim(),
+  check('group', 'Validación: ID del grupo debe ser válido').isMongoId().not().isEmpty().trim(),
   check('name', 'Campo requerido: Nombre de materia').not().isEmpty(),
   check('code', 'Campo requerido: Código de materia').not().isEmpty(),
-  check('independent', 'Validación: Campo independiente debe ser verdadero o falso').isBoolean().not().isEmpty(),
-  check('includeInStatistics', 'Validación: Campo incluir en estadísticas debe ser verdadero o falso').isBoolean().not().isEmpty(),
-  check('type', 'Validación: ID de tipo debe ser válido').isMongoId().not().isEmpty(),
-  check('area', 'Campo requerido: Área de materia').not().isEmpty(),
+  check('type', 'Campo requerido:  Tipo (materia/area)').not().isEmpty(),
+  check('areaCode', 'Campo requerido: Área de materia').not().isEmpty(),
   validateFields
 ], createSubject);             // POST /api/subjects
 
 router.put('/:id',[
   check('id', 'Validación: ID de materia debe ser válido').isMongoId().not().isEmpty(),
-  check('school', 'Validación: ID de colegio debe ser válido').isMongoId().not().isEmpty().trim(),
+  check('group', 'Validación: ID del grupo debe ser válido').isMongoId().not().isEmpty().trim(),
   check('name', 'Campo requerido: Nombre de materia').not().isEmpty(),
   check('code', 'Campo requerido: Código de materia').not().isEmpty(),
-  check('independent', 'Validación: Campo independiente debe ser verdadero o falso').isBoolean().not().isEmpty(),
-  check('includeInStatistics', 'Validación: Campo incluir en estadísticas debe ser verdadero o falso').isBoolean().not().isEmpty(),
-  check('type', 'Validación: ID de tipo debe ser válido').isMongoId().not().isEmpty(),
-  check('area', 'Campo requerido: Área de materia').not().isEmpty(),
+  check('type', 'Campo requerido:  Tipo (materia/area)').not().isEmpty(),
+  check('areaCode', 'Campo requerido: Área de materia').not().isEmpty(),
   validateFields
 ], updateSubject);                     // PUT /api/subjects/:id
 
